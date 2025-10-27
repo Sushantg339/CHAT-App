546b963447e863c538ba7d61993d026dd6a4ac38
Sushant Gupta <sushantg339@gmail.com>
2025-10-28 00:04:21 +0530

    update profile endpoint added

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from 'express'
-import { signupController , loginController, logoutController } from '../controllers/auth.controller.js'
+import { signupController , loginController, logoutController , updateProfileController} from '../controllers/auth.controller.js'
+import { authMiddleware } from '../middlewares/auth.middleware.js'
 
 const router = express.Router()
 
@@ -8,5 +9,8 @@ router.post('/signup' , signupController)
 router.post('/login' , loginController)
 
 router.post('/logout' , logoutController)
+router.post('/update-profile' ,authMiddleware, updateProfileController)
+
+router.get('/check' , authMiddleware , (req,res)=>{res.status(200).json( req.user)})
 
 export default router
